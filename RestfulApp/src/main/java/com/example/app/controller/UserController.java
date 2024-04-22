3a62b0fdb2872332f2bdf83b1067734d6f7bedb1
Shalini R <jayanthishalini12@gmail.com>
2024-04-22 10:56:13 +0530

    Handled Get method and returned a response

--- RestfulApp/src/main/java/com/example/app/controller/UserController.java
+++ RestfulApp/src/main/java/com/example/app/controller/UserController.java
@@ -1,20 +1,46 @@
 package com.example.app.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.app.model.response.UserRest;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
 	@GetMapping
-	public String getUser(){
-		return "get user was called";
+	public String getUser(@RequestParam(value="page", defaultValue="1") int page, 
+			@RequestParam(value="limit", defaultValue="50") int limit,
+			@RequestParam(value="sort", defaultValue="desc", required=false) String sort){
+	
+		return "get user was called with page = " + page + " and limit = " + limit + " and sort = " + sort;
 	}
+
+	@GetMapping(path="/{userId}",
+			produces = { 
+					MediaType.APPLICATION_XML_VALUE, 
+					MediaType.APPLICATION_JSON_VALUE
+					})
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId){
+		
+		UserRest returnValue = new UserRest();
+		returnValue.setEmail("test@test.com");
+		returnValue.setFirstName("Shalini");
+		returnValue.setLastName("Ramanujam");
+		
+		return new ResponseEntity<UserRest>( HttpStatus.BAD_REQUEST);
+	}
+
 	
 	@PostMapping
 	public String createUser(){
